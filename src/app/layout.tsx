62b21173a2e48691186d30267204781b7e0e8495
levinhky <chansolle@gmail.com>
2023-08-22 17:40:28 +0700

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-// Font files can be colocated inside of `pages`
-
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +16,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
