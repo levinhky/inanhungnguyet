15427fb183afb1d75e9fb2348a2843bf96d9480b
levinhky <chansolle@gmail.com>
2023-08-24 06:57:53 +0700

    up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,23 +13,30 @@ const [searchValue, setSearchValue] = useState('');
       <div className="flex justify-between items-center">
         <div className="main-logo">
           <Image
-            src={"/logo-nobg.svg"}
+            src={"/logo_nobg.svg"}
             alt="inanhungnguyetlogo"
-            width={120}
-            height={120}
+            width={150}
+            height={150}
           />
         </div>
-        <div className="block-search">
-          <div className="field-search">
+        <div className="block-search h-[40px] relative">
+          <div className="field-search w-[500px]">
             <input
               type="text"
               onInput={(e) => setSearchValue(e.currentTarget.value)}
               value={searchValue}
-              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-[#0050b3] focus:ring-[#0050b3] block w-full rounded-md sm:text-sm focus:ring-1"
+              className="px-3 py-2 bg-[#F0F2F5] border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-[#0050b3] block w-full rounded-full sm:text-sm"
               placeholder="Tìm sản phẩm mong muốn ..."
             />
           </div>
-          <button className="action-search"></button>
+          <button className="action-search absolute top-[5px] right-[15px]">
+          <Image
+            src={"/icons/search.svg"}
+            alt="inanhungnguyetlogo"
+            width={25}
+            height={25}
+          />
+          </button>
         </div>
         <div className="links">
           <button className="login">
