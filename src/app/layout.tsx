fe121ce808e81785331d7d96443c2a383f67e2dc
levinhky <chansolle@gmail.com>
2023-09-05 16:01:22 +0700

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,12 @@ import Header from "@/components/Header";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import { Providers } from "@/redux/provider";
-
-const metadataContent = {
-  title: "In Ấn Hùng Nguyệt",
-  description:
-    "Bạn đang tìm một địa chỉ in ly nhựa dùng một lần cho quán cafe, trà sữa uy tín, chất lượng và giá cả hợp lý. Liên hệ ngay cho chúng tôi để có giá tốt nhất thị trường hiện nay. In Ấn Hùng Nguyệt chuyên in các loại bao bì, ly nhựa, ly trà sữa, phong bao ở Kỳ Anh Hà Tĩnh. In ly nhựa giá cực rẻ còn MIỄN PHÍ thiết kế logo - MIỄN PHÍ giao hàng - MIỄN PHÍ tư vấn giải pháp xây dựng bộ nhận diện thương hiệu thông qua bao bì sản phẩm.",
-};
+import { metadataContent } from "@/data/arrays";
 
 export const metadata: Metadata = {
   title: metadataContent.title,
   description: metadataContent.description,
+  keywords: metadataContent.keywords,
   openGraph: metadataContent,
 };
 
