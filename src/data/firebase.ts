e8e0d6136fd9c3dd925325606f86735e8a7f0fd6
levinhky <chansolle@gmail.com>
2023-10-16 08:43:36 +0700

    up

--- src/data/firebase.ts
+++ src/data/firebase.ts
@@ -14,5 +14,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+let analytics;
 export const auth = getAuth(app);
+if (app.name && typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+}
