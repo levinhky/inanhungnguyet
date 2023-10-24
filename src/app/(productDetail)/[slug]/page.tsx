798cabf9d92a5cd33a25da22393f2fed5da6689b
levinhky <chansolle@gmail.com>
2023-10-24 20:20:34 +0700

    up

--- src/app/(productDetail)/[slug]/page.tsx
+++ src/app/(productDetail)/[slug]/page.tsx
@@ -4,6 +4,7 @@ import BreadCrumb from "@/components/BreadCrumb";
 import React, { useEffect, useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import Loading from "@/components/Loading";
 
 type Props = {
   params: {
@@ -31,11 +32,12 @@ export default function page({ params }: Props) {
     __v: 0,
   });
   const [slugName, setSlugName] = useState<string>(slug);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getProduct = async () => {
       const response = await fetch(
-        process.env.NEXT_PUBLIC_BASE_URL +'api/products/' + slug,
+        process.env.NEXT_PUBLIC_BASE_URL + "api/products/" + slug,
         { cache: "no-store" }
       );
       const fetchedProduct = await response.json();
@@ -45,7 +47,8 @@ export default function page({ params }: Props) {
         Object.values(fetchedProduct).some((value) => value !== "")
       ) {
         setProduct(fetchedProduct.product);
-        setSlugName(fetchedProduct.product.name)
+        setSlugName(fetchedProduct.product.name);
+        setIsLoading(false);
       }
     };
 
@@ -53,68 +56,75 @@ export default function page({ params }: Props) {
   }, [slug]);
 
   return (
-    <div className="container mx-auto">
-      <BreadCrumb slug={slug} slugName={slugName} />
-      <section id="content">
-        <div className="flex flex-wrap rock:flex-nowrap">
-          <div className="images rock:w-1/2 rock:mr-10">
-            <Carousel className="rock:w-[600px]" showStatus={false}>
-              {product.thumbs &&
-                product.thumbs.map((thumb, index) => (
-                  <div key={thumb}>
-                    <img
-                      src={thumb}
-                      className="select-none rounded-lg"
-                      alt={thumb}
-                    />
-                  </div>
-                ))}
-            </Carousel>
-          </div>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="container mx-auto">
+          <BreadCrumb slug={slug} slugName={slugName} />
+          <section id="content">
+            <div className="flex flex-wrap rock:flex-nowrap">
+              <div className="images rock:w-1/2 rock:mr-10">
+                <Carousel className="rock:w-[600px]" showStatus={false}>
+                  {product.thumbs &&
+                    product.thumbs.map((thumb, index) => (
+                      <div key={thumb}>
+                        <img
+                          src={thumb}
+                          className="select-none rounded-lg"
+                          alt={thumb}
+                        />
+                      </div>
+                    ))}
+                </Carousel>
+              </div>
 
-          <div className="info text-center rock:text-left w-full">
-            <h1 className="font-bold text-2xl rock:my-5 mb-3">
-              {product.name}
-            </h1>
+              <div className="info text-center rock:text-left w-full">
+                <h1 className="font-bold text-2xl rock:my-5 mb-3">
+                  {product.name}
+                </h1>
 
-            <p className="rock:mb-5 mb-3">
-              <span className="font-bold">Tình trạng :</span> {product.status === true ? "Còn hàng" : "Hết hàng"}
-            </p>
+                <p className="rock:mb-5 mb-3">
+                  <span className="font-bold">Tình trạng :</span>{" "}
+                  {product.status === true ? "Còn hàng" : "Hết hàng"}
+                </p>
 
-            <h2 className="text-[var(--gray-text)] text-sm rock:mb-5 mb-3">
-              SKU: {product.sku}
-            </h2>
+                <h2 className="text-[var(--gray-text)] text-sm rock:mb-5 mb-3">
+                  SKU: {product.sku}
+                </h2>
 
-            <button className="text-white bg-[var(--blue-text)] rock:hover:bg-[var(--blue)] rounded-full text-base py-2 px-5 h-12 min-w-[200px]">
-              Nhận báo giá
-            </button>
-          </div>
+                <button className="text-white bg-[var(--blue-text)] rock:hover:bg-[var(--blue)] rounded-full text-base py-2 px-5 h-12 min-w-[200px]">
+                  Nhận báo giá
+                </button>
+              </div>
+            </div>
+
+            <div className="additonal-infomation rock:w-1/2">
+              <h3 className="rock:text-center text-left font-bold text-base pb-5 rock:pt-0 pt-7 border-b-[3px] border-b-[var(--blue)]">
+                Thông tin thêm
+              </h3>
+
+              <ul className="info mt-5 list-disc list-inside">
+                <li>
+                  <b>Dung tích:</b> {product.capacity}{" "}
+                </li>
+                <li>
+                  <b>Màu sắc:</b> {product.color}
+                </li>
+                <li>
+                  <b>Đặc tính:</b> {product.characteristics}
+                </li>
+                <li>
+                  <b>Mẫu mã:</b> {product.design}
+                </li>
+                <li>
+                  <b>Công dụng:</b> {product.uses}{" "}
+                </li>
+              </ul>
+            </div>
+          </section>
         </div>
-
-        <div className="additonal-infomation rock:w-1/2">
-          <h3 className="rock:text-center text-left font-bold text-base pb-5 rock:pt-0 pt-7 border-b-[3px] border-b-[var(--blue)]">
-            Thông tin thêm
-          </h3>
-
-          <ul className="info mt-5 list-disc list-inside">
-            <li>
-              <b>Dung tích:</b> {product.capacity}{" "}
-            </li>
-            <li>
-              <b>Màu sắc:</b> {product.color}
-            </li>
-            <li>
-              <b>Đặc tính:</b> {product.characteristics}
-            </li>
-            <li>
-              <b>Mẫu mã:</b> {product.design}
-            </li>
-            <li>
-              <b>Công dụng:</b> {product.uses}{" "}
-            </li>
-          </ul>
-        </div>
-      </section>
-    </div>
+      )}
+    </>
   );
 }
--- src/app/danh-sach-san-pham/page.tsx
+++ src/app/danh-sach-san-pham/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import BreadCrumb from "@/components/BreadCrumb";
+import Loading from "@/components/Loading";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
@@ -9,115 +10,119 @@ type Props = {};
 export default function ProductListing({}: Props) {
   const [categories, setCategories] = useState<[CategoryAttributes]>();
   const [products, setProducts] = useState<[ProductAttributes]>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getCategories = async () => {
-      const res = await fetch(
+      const categoryResponse = await fetch(
         process.env.NEXT_PUBLIC_BASE_URL + "api/categories"
       );
-      const data = await res.json();
-      data && setCategories(data.categories);
-    };
-
-    const getProducts = async () => {
-      const res = await fetch(
+      const productResponse = await fetch(
         process.env.NEXT_PUBLIC_BASE_URL + "api/products"
       );
-      const data = await res.json();
-      data && setProducts(data.products);
+
+      const categories = await categoryResponse.json();
+      categories && setCategories(categories.categories);
+
+      const products = await productResponse.json();
+      products && setProducts(products.products);
+      setIsLoading(false);
     };
 
     getCategories();
-    getProducts();
   }, []);
 
   return (
-    <div className="container mx-auto rock:text-base text-sm">
-      <style jsx>{`
-        article {
-          box-shadow: 0 1px 3px rgba(0, 0, 0, 0.15);
-        }
-        select {
-          background: url(/icons/select.svg);
-          background-repeat: no-repeat;
-          background-position: right;
-        }
-      `}</style>
-      <BreadCrumb />
-      <h1 className="font-bold rock:text-2xl text-xl text-center">
-        Danh sách sản phẩm{" "}
-        <span className="count text-[var(--gray-text)] text-xs rock:text-sm font-medium">
-          ({products?.length} sản phẩm )
-        </span>
-      </h1>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="container mx-auto rock:text-base text-sm">
+          <style jsx>{`
+            article {
+              box-shadow: 0 1px 3px rgba(0, 0, 0, 0.15);
+            }
+            select {
+              background: url(/icons/select.svg);
+              background-repeat: no-repeat;
+              background-position: right;
+            }
+          `}</style>
+          <BreadCrumb />
+          <h1 className="font-bold rock:text-2xl text-xl text-center">
+            Danh sách sản phẩm{" "}
+            <span className="count text-[var(--gray-text)] text-xs rock:text-sm font-medium">
+              ({products?.length} sản phẩm )
+            </span>
+          </h1>
 
-      <section className="flex flex-col-reverse rock:flex-row mt-7">
-        <article className="p-5 rounded-xl rock:w-64 w-full h-fit rock:mt-0 mt-5">
-          <h3 className="font-bold pb-2.5 mb-5 border-b border-b-[#ccc] uppercase">
-            Danh mục
-          </h3>
-          <ul>
-            {categories?.length &&
-              categories?.map((category) => (
-                <li key={category._id} className="mb-1">
-                  <Link
-                    className="rock:hover:text-[var(--blue-text)] rock:ease-linear rock:delay-75"
-                    href="aa"
-                  >
-                    {category.name} ({category.productsInCategory.length})
-                  </Link>
-                </li>
-              ))}
-          </ul>
-        </article>
+          <section className="flex flex-col-reverse rock:flex-row mt-7">
+            <article className="p-5 rounded-xl rock:w-64 w-full h-fit rock:mt-0 mt-5">
+              <h3 className="font-bold pb-2.5 mb-5 border-b border-b-[#ccc] uppercase">
+                Danh mục
+              </h3>
+              <ul>
+                {categories?.length &&
+                  categories?.map((category) => (
+                    <li key={category._id} className="mb-1">
+                      <Link
+                        className="rock:hover:text-[var(--blue-text)] rock:ease-linear rock:delay-75"
+                        href="aa"
+                      >
+                        {category.name} ({category.productsInCategory.length})
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+            </article>
 
-        <aside className="rock:ml-10">
-          <div className="toolbar-sorter mb-7">
-            <label htmlFor="options" className="text-xs rock:text-sm">
-              Sắp xếp theo:
-            </label>
-            <select
-              id="options"
-              defaultValue={'US'}
-              className="mx-3 min-w-[150px] py-2 pr-7 appearance-none pl-3 text-xs rock:text-sm border rock:cursor-pointer rounded-lg font-bold focus:border-[#ccc]"
-            >
-              <option value="US">
-                Tên sản phẩm (A tới Z)
-              </option>
-              <option value="CA">Tên sản phẩm (Z tới A) </option>
-              <option value="FR">Xem nhiều </option>
-            </select>
-          </div>
+            <aside className="rock:ml-10">
+              <div className="toolbar-sorter mb-7">
+                <label htmlFor="options" className="text-xs rock:text-sm">
+                  Sắp xếp theo:
+                </label>
+                <select
+                  id="options"
+                  defaultValue={"US"}
+                  className="mx-3 min-w-[150px] py-2 pr-7 appearance-none pl-3 text-xs rock:text-sm border rock:cursor-pointer rounded-lg font-bold focus:border-[#ccc]"
+                >
+                  <option value="US">Tên sản phẩm (A tới Z)</option>
+                  <option value="CA">Tên sản phẩm (Z tới A) </option>
+                  <option value="FR">Xem nhiều </option>
+                </select>
+              </div>
 
-          <div className="products grid rock:grid-cols-4 grid-cols-2 rock:gap-7 gap-4">
-            {products?.length &&
-              products.map((product, i) => (
-                <div key={product._id} className="product">
-                  <div className="thumb">
-                    <Link href={product.slug}>
-                      <img
-                        src={product.thumbs[0]}
-                        className="select-none rounded-lg"
-                        alt={product.name}
-                      />
-                    </Link>
-                  </div>
-                  <div className="info px-5 py-3">
-                    <h2 className="contact uppercase text-[var(--blue)] font-bold my-2 py-1 border-b border-[var(--gray)]">
-                      liên hệ chúng tôi...
-                    </h2>
-                    <h1 className="name font-normal line-clamp-2">
-                      <Link href={product.slug}>{product.name}</Link>
-                    </h1>
-                    <h3 className="sku uppercase text-[var(--gray-text)] text-sm">
-                      SKU: {product.sku}
-                    </h3>
-                  </div>
-                </div>
-              ))}
-          </div>
-        </aside>
-      </section>
-    </div>
+              <div className="products grid rock:grid-cols-4 grid-cols-2 rock:gap-7 gap-4">
+                {products?.length &&
+                  products.map((product, i) => (
+                    <div key={product._id} className="product">
+                      <div className="thumb">
+                        <Link href={product.slug}>
+                          <img
+                            src={product.thumbs[0]}
+                            className="select-none rounded-lg"
+                            alt={product.name}
+                          />
+                        </Link>
+                      </div>
+                      <div className="info px-5 py-3">
+                        <h2 className="contact uppercase text-[var(--blue)] font-bold my-2 py-1 border-b border-[var(--gray)]">
+                          liên hệ chúng tôi...
+                        </h2>
+                        <h1 className="name font-normal line-clamp-2">
+                          <Link href={product.slug}>{product.name}</Link>
+                        </h1>
+                        <h3 className="sku uppercase text-[var(--gray-text)] text-sm">
+                          SKU: {product.sku}
+                        </h3>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </aside>
+          </section>
+        </div>
+      )}
+    </>
   );
 }
