9527b95659ceed30c98996c9c7699cda40a76c0c
levinhky <chansolle@gmail.com>
2024-02-15 15:04:24 +0700

    update trans

--- src/app/[locale]/chinh-sach-van-chuyen-thanh-toan/page.tsx
+++ src/app/[locale]/chinh-sach-van-chuyen-thanh-toan/page.tsx
@@ -13,23 +13,38 @@ export default function ShippingAndPaymentPoliciesPage({}: Props) {
   return (
     <div className="container mx-auto rock:text-base text-sm">
       <BreadCrumb />
-      <h3 className="font-bold rock:text-2xl text-xl">{t("policyOf", { storeName: t_global("storeName") })}</h3>
+      <h3 className="font-bold rock:text-2xl text-xl">
+        {t("policyOf", { storeName: t_global("storeName") })}
+      </h3>
 
       <section>
-        <h6 className="rock:my-5 my-3  font-semibold">{t("interactiveService.headline")}</h6>
+        <h6 className="rock:my-5 my-3  font-semibold">
+          {t("interactiveService.headline")}
+        </h6>
 
-        <h5 className="mb-2 ">{t("interactiveService.description")}</h5>
+        <h5 className="mb-2 ">
+          {t("interactiveService.description", {
+            storeName: t_global("storeName"),
+          })}
+        </h5>
 
         <ul>
           <li className="mb-2">
             &bull; Website:
-            <Link className="text-[var(--blue)] ml-1 rock:hover:opacity-80 rock:ease-in-out" href={"/"}>
+            <Link
+              className="text-[var(--blue)] ml-1 rock:hover:opacity-80 rock:ease-in-out"
+              href={"/"}
+            >
               https://inanhungnguyet.com
             </Link>
           </li>
           <li className="mb-2">
             &bull; {t_global("social")}:
-            <Link href={"https://www.facebook.com/levinhhoa.levinhhoa"} target="_blank" rel="noopener noreferrer">
+            <Link
+              href={"https://www.facebook.com/levinhhoa.levinhhoa"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FacebookOutlined className="text-2xl ml-1 text-blue-700" />
             </Link>
           </li>
@@ -59,32 +74,53 @@ export default function ShippingAndPaymentPoliciesPage({}: Props) {
       </section>
 
       <section>
-        <h6 className="rock:my-5 my-3  font-semibold">{t("deliveryAndOrderingTime.headline")}</h6>
+        <h6 className="rock:my-5 my-3  font-semibold">
+          {t("deliveryAndOrderingTime.headline")}
+        </h6>
 
         <ul>
           <li className="mb-2 font-semibold">
-            &bull; {t_global("orderFrom")}:<p className="font-normal my-2">{t("deliveryAndOrderingTime.orderTime")}</p>
+            &bull; {t_global("orderFrom")}:
+            <p className="font-normal my-2">
+              {t("deliveryAndOrderingTime.orderTime")}
+            </p>
           </li>
           <li className="mb-2 font-semibold">
             &bull; {t_global("deliveryFrom")}:
-            <p className="font-normal my-2">{t("deliveryAndOrderingTime.deliveryMorning")}</p>
-            <p className="font-normal mt-2 mb-5">{t("deliveryAndOrderingTime.deliveryAfternoon")}</p>
+            <p className="font-normal my-2">
+              {t("deliveryAndOrderingTime.deliveryMorning")}
+            </p>
+            <p className="font-normal mt-2 mb-5">
+              {t("deliveryAndOrderingTime.deliveryAfternoon")}
+            </p>
             <ul>
-              <li className="mb-2 font-normal">&bull; {t("deliveryAndOrderingTime.deliverySchedule")}</li>
-              <li className="mb-2 font-normal">&bull; {t("deliveryAndOrderingTime.objectiveCase")}</li>
+              <li className="mb-2 font-normal">
+                &bull; {t("deliveryAndOrderingTime.deliverySchedule")}
+              </li>
+              <li className="mb-2 font-normal">
+                &bull; {t("deliveryAndOrderingTime.objectiveCase")}
+              </li>
             </ul>
-            <p className="font-normal my-2">{t("deliveryAndOrderingTime.highBuildingCase")}</p>
+            <p className="font-normal my-2">
+              {t("deliveryAndOrderingTime.highBuildingCase")}
+            </p>
           </li>
         </ul>
       </section>
 
       <section>
-        <h6 className="rock:my-5 my-3  font-semibold">{t("paymentMethod.headline")}</h6>
+        <h6 className="rock:my-5 my-3  font-semibold">
+          {t("paymentMethod.headline")}
+        </h6>
 
         <ul>
           <li className="mb-2 font-normal">&bull; {t("paymentMethod.cod")}</li>
-          <li className="mb-2 font-normal">&bull; {t("paymentMethod.banking")}</li>
-          <li className="mb-2 font-normal">&bull; {t("paymentMethod.outsideProvince")}</li>
+          <li className="mb-2 font-normal">
+            &bull; {t("paymentMethod.banking")}
+          </li>
+          <li className="mb-2 font-normal">
+            &bull; {t("paymentMethod.outsideProvince")}
+          </li>
         </ul>
       </section>
     </div>
