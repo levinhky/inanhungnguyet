58f71d1c35820b2e9bf63dcb3bfc545554397db0
levinhky <chansolle@gmail.com>
2024-02-18 17:32:57 +0700

    Update Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,25 @@ export default function RootLayout({
   return (
     <html lang="vi">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="favicon/favicon.ico" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="favicon/site.webmanifest" />
       </head>
       <body suppressHydrationWarning={true}>
         <Providers>
