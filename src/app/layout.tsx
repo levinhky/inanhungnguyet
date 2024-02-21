b3f71b528459611bd788c72c1a8917012084ebb4
levinhky <ky.le@snaptec.co>
2024-02-21 10:44:55 +0700

    Update Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,26 +27,26 @@ export default function RootLayout({
   return (
     <html lang="vi">
       <head>
-        <link rel="icon" href="favicon/favicon.ico" sizes="any" />
-        <link rel="shortcut icon" href="favicon/favicon.ico" sizes="any" />
+        <link rel="icon" href="favicon.ico" sizes="any" />
+        <link rel="shortcut icon" href="favicon.ico" sizes="any" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="favicon/apple-touch-icon.png"
+          href="apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="favicon/favicon-32x32.png"
+          href="favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="favicon/favicon-16x16.png"
+          href="favicon-16x16.png"
         />
-        <link rel="manifest" href="favicon/site.webmanifest" />
+        <link rel="manifest" href="site.webmanifest" />
       </head>
       <body suppressHydrationWarning={true}>
         <Providers>
