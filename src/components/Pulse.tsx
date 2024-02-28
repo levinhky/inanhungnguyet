aedb298f21bc7d735e79bd50c8885042918ba14f
levinhky <chansolle@gmail.com>
2024-02-28 21:39:03 +0700

    Update Pulse

--- src/components/Pulse.tsx
+++ src/components/Pulse.tsx
@@ -1,13 +1,12 @@
-"use client";
 import { pulseIcons } from "@/data/arrays";
 import Image from "next/image";
 import Link from "next/link";
 
 const PulseIcon: React.FC<{ icon: PulseIcon }> = ({ icon }) => (
   <div
-    className={`circle pulse rock:w-11 rock:h-11 w-10 h-10 ${icon.alt === "phone" ? "mb-5" : "mb-8"} ${
-      icon.alt === "phone" ? "phone-ring bg-[#66ff99]" : ""
-    }`}
+    className={`pulse rounded-full shadow-sm rock:w-11 rock:h-11 w-10 h-10 ${
+      icon.alt === "phone" ? "mb-5" : "mb-8"
+    } ${icon.alt === "phone" ? "phone-ring bg-[#66ff99]" : ""}`}
   >
     <Link target="_blank" rel="nofollow" href={icon.href}>
       <Image width={100} height={100} src={icon.src} alt={icon.alt} />
@@ -17,12 +16,6 @@ const PulseIcon: React.FC<{ icon: PulseIcon }> = ({ icon }) => (
 
 const Pulse: React.FC = () => (
   <section className="fixed rock:left-7 left-3 bottom-0 z-10">
-    <style jsx>{`
-      .circle {
-        border-radius: 50%;
-        box-shadow: 0px 0px 1px 1px #0000001a;
-      }
-    `}</style>
     {pulseIcons.map((icon) => (
       <PulseIcon key={icon.alt} icon={icon} />
     ))}
