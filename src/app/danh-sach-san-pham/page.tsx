92018fd7ee49b8f73e98f7d69e5592d21f7ff422
levinhky <ky.le@snaptec.co>
2024-03-01 14:19:02 +0700

    Add suspense

--- src/app/danh-sach-san-pham/page.tsx
+++ src/app/danh-sach-san-pham/page.tsx
@@ -1,25 +1,28 @@
+import Loading from "@/components/Loading";
 import ProductList from "@/components/ProductList";
 import useCategoryList from "@/services/hooks/category/useCategoryList";
 import useProductList from "@/services/hooks/product/useProductList";
+import { Suspense } from "react";
 
 type Props = {
   searchParams?: { [key: string]: string };
 };
 
 export default async function ProductListingPage({ searchParams }: Props) {
-  const { products, totalCount, totalPages, currentPage, limit, sortParam } =
-    await useProductList({ searchParams });
+  const { products, totalCount, totalPages, currentPage, limit, sortParam } = await useProductList({ searchParams });
   const categories = await useCategoryList();
 
   return (
-    <ProductList
-      categories={categories}
-      products={products}
-      totalPages={totalPages}
-      totalCount={totalCount}
-      currentPage={currentPage}
-      limit={limit}
-      sortParam={sortParam}
-    />
+    <Suspense key={currentPage} fallback={<Loading />}>
+      <ProductList
+        categories={categories}
+        products={products}
+        totalPages={totalPages}
+        totalCount={totalCount}
+        currentPage={currentPage}
+        limit={limit}
+        sortParam={sortParam}
+      />
+    </Suspense>
   );
 }
