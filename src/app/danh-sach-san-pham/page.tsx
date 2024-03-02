b6d68070d6fc2c3c11f986023617ecf62805c8d7
levinhky <ky.le@snaptec.co>
2024-03-02 10:18:37 +0700

    Update

--- src/app/danh-sach-san-pham/page.tsx
+++ src/app/danh-sach-san-pham/page.tsx
@@ -1,8 +1,6 @@
-import Loading from "@/components/Loading";
 import ProductList from "@/components/ProductList";
 import useCategoryList from "@/services/hooks/category/useCategoryList";
 import useProductList from "@/services/hooks/product/useProductList";
-import { Suspense } from "react";
 
 type Props = {
   searchParams?: { [key: string]: string };
@@ -13,16 +11,14 @@ export default async function ProductListingPage({ searchParams }: Props) {
   const categories = await useCategoryList();
 
   return (
-    <Suspense key={JSON.stringify(searchParams)} fallback={<Loading />}>
-      <ProductList
-        categories={categories}
-        products={products}
-        totalPages={totalPages}
-        totalCount={totalCount}
-        currentPage={currentPage}
-        limit={limit}
-        sortParam={sortParam}
-      />
-    </Suspense>
+    <ProductList
+      categories={categories}
+      products={products}
+      totalPages={totalPages}
+      totalCount={totalCount}
+      currentPage={currentPage}
+      limit={limit}
+      sortParam={sortParam}
+    />
   );
 }
